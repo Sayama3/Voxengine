8ae54daf003452646db1b5b86d2516b66869afa0
Iannis Poquet <39456336+Sayama3@users.noreply.github.com>
2023-07-04 19:10:14 +0200

    Define _USE_MATH_DEFINES ifndef

--- include/library.hpp
+++ include/library.hpp
@@ -1,6 +1,11 @@
 #ifndef VOXYMORECORE_LIBRARY_HPP
 #define VOXYMORECORE_LIBRARY_HPP
 
+// Defining the "_USE_MATH_DEFINES" for visual studio in the main hpp
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif
+
 void hello();
 
 #endif //VOXYMORECORE_LIBRARY_HPP
