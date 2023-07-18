e9baaf653923db02fb543e49a34162e96ad0de64
Iannis Poquet <39456336+Sayama3@users.noreply.github.com>
2023-07-18 11:19:43 +0200

    Add Layers

--- include/Events/Event.hpp
+++ include/Events/Event.hpp
@@ -40,6 +40,8 @@ namespace Voxymore::Core {
         {
             friend class EventDispatcher;
         public:
+            bool m_Handled = false;
+
             virtual EventType GetEventType() const = 0;
             virtual const char* GetName() const = 0;
             virtual int GetCategoryFlags() const = 0;
@@ -48,8 +50,6 @@ namespace Voxymore::Core {
             {
                 return GetCategoryFlags() & category;
             }
-        protected:
-            bool m_Handled = false;
         };
 
         class EventDispatcher
---
+++ include/Layers/Layer.hpp
@@ -0,0 +1,27 @@
+//
+// Created by ianpo on 18/07/2023.
+//
+
+#pragma once
+
+#include "Events/Event.hpp"
+
+// We shouldn't search layer by name!!
+
+namespace Voxymore::Core::Layers {
+    class VXM_API Layer {
+    public:
+        Layer(const std::string& name = "Layer");
+        virtual ~Layer();
+
+        virtual void OnAttach() {}
+        virtual void OnDetach() {}
+        virtual void OnUpdate() {}
+        virtual void OnEvent(Events::Event& event) {}
+
+        inline const std::string& GetName() const { return m_DebugName; }
+    protected:
+        std::string m_DebugName;
+    };
+
+} // Layers
