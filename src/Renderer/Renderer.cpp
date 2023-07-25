c2ef5938019e7bc1daafd3db758b94d2e42d67ff
Iannis Poquet <39456336+Sayama3@users.noreply.github.com>
2023-07-25 16:43:18 +0200

    Add transform (ModelMatrix) in the shaders.

--- src/Renderer/Renderer.cpp
+++ src/Renderer/Renderer.cpp
@@ -16,13 +16,15 @@ namespace Voxymore::Core {
 		s_Camera = nullptr;
     }
 
-    void Renderer::Submit(std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray> &vertexArray) {
+    void Renderer::Submit(std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray> &vertexArray, const glm::mat4& transform) {
 		if(s_Camera == nullptr) {
 			VXM_CORE_ERROR("No valid camera set, begin the scene with a camera before submitting meshes.");
 			return;
 		}
 		shader->Bind();
-		shader->SetUniformMat4("u_ViewProjectionMatrix", s_Camera->GetViewProjectionMatrix());
+        //TODO: Set the view projection matrix once per frame not once per model drawn.
+        shader->SetUniformMat4("u_ViewProjectionMatrix", s_Camera->GetViewProjectionMatrix());
+        shader->SetUniformMat4("u_Transform", transform);
         vertexArray->Bind();
         RenderCommand::DrawIndexed(vertexArray);
     }
