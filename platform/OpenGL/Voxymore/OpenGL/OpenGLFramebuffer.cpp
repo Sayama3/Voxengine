2b76b470c19485d0b3dfb49e2458600999d72f99
Iannis Poquet <39456336+Sayama3@users.noreply.github.com>
2023-08-21 21:06:47 +0200

    Fix runtime error

--- platform/OpenGL/Voxymore/OpenGL/OpenGLFramebuffer.cpp
+++ platform/OpenGL/Voxymore/OpenGL/OpenGLFramebuffer.cpp
@@ -40,7 +40,7 @@ namespace Voxymore::Core {
 
         glCreateTextures(GL_TEXTURE_2D, 1, &m_DepthAttachment);
         glBindTexture(GL_TEXTURE_2D, m_DepthAttachment);
-        glTexStorage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, m_Specification.Width, m_Specification.Height);
+        glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH24_STENCIL8, m_Specification.Width, m_Specification.Height);
 //        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, m_Specification.Width, m_Specification.Height, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, nullptr);
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_DepthAttachment, 0);
 
