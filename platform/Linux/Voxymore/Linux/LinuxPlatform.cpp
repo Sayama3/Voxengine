c6703995b0b6318faa608ca419532324743354b4
Iannis Poquet <39456336+Sayama3@users.noreply.github.com>
2023-08-29 19:35:17 +0200

    Fix SaveFile dialog

--- platform/Linux/Voxymore/Linux/LinuxPlatform.cpp
+++ platform/Linux/Voxymore/Linux/LinuxPlatform.cpp
@@ -16,8 +16,7 @@ namespace Voxymore::Core {
 
 	std::string FileDialogs::SaveFile(const std::vector<std::string>& filter)
 	{
-		std::vector<std::string> result = pfd::open_file("Select a file", ".", filter).result();
-		if(result.empty()) return std::string();
-		else return result[0];
+		std::string result = pfd::save_file("Select a file", ".", filter).result();
+		return result;
 	}
 }
