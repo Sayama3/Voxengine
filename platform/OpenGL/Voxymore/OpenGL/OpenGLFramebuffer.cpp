b72930efef55af4ec17c803df4639cd21e54a963
Iannis Poquet <39456336+Sayama3@users.noreply.github.com>
2023-09-17 15:23:22 +0200

    Rework framebuffer

--- platform/OpenGL/Voxymore/OpenGL/OpenGLFramebuffer.cpp
+++ platform/OpenGL/Voxymore/OpenGL/OpenGLFramebuffer.cpp
@@ -10,11 +10,96 @@ namespace Voxymore::Core {
 
     //TODO: retrieve the max depending on the GPU capabilities.
     static const uint32_t s_MaxFramebufferSize = 8192;
+	namespace Utils
+	{
+		static GLenum TextureTarget(bool multisampled)
+		{
+			return multisampled ? GL_TEXTURE_2D_MULTISAMPLE : GL_TEXTURE_2D;
+		}
+
+		static void CreateTextures(bool multisampled, uint32_t* outIds, uint32_t count)
+		{
+			glCreateTextures(TextureTarget(multisampled), count, outIds);
+		}
+
+		static void BindTexture(bool multisampled, uint32_t textureId)
+		{
+			glBindTexture(TextureTarget(multisampled), textureId);
+		}
+
+		static void AttachmentColorTexture(uint32_t id, uint32_t samples, GLenum textureFormat, uint32_t width, uint32_t height, int index)
+		{
+			bool multisample = samples > 1;
+			if(multisample)
+			{
+				glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, samples, textureFormat, width, height, GL_FALSE);
+			}
+			else
+			{
+				glTexImage2D(GL_TEXTURE_2D, 0, textureFormat, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			}
+
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + index, TextureTarget(multisample), id, 0);
+		}
+
+		static void AttachmentDepthTexture(uint32_t id, uint32_t samples, GLenum textureFormat, GLenum attachmentType, uint32_t width, uint32_t height)
+		{
+			bool multisample = samples > 1;
+			if(multisample)
+			{
+				glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, samples, textureFormat, width, height, GL_FALSE);
+			}
+			else
+			{
+				glTexStorage2D(GL_TEXTURE_2D, 1, textureFormat, width, height);
+
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			}
+
+			glFramebufferTexture2D(GL_FRAMEBUFFER, attachmentType, TextureTarget(multisample), id, 0);
+		}
+
+		static bool IsDepthFormat(FramebufferTextureFormat format)
+		{
+			switch (format) {
+				// Other Depth format.
+				case FramebufferTextureFormat::DEPTH24STENCIL8:
+					return true;
+				default:
+					return false;
+			}
+			return false;
+		}
+	}
 
     OpenGLFramebuffer::OpenGLFramebuffer(const FramebufferSpecification& specification) : m_Specification(specification)
     {
         VXM_CORE_ASSERT(m_Specification.Width > 0 && m_Specification.Width <= s_MaxFramebufferSize, "Framebufffer width '{0}' must be superior to 0 and inferior to {1}.", m_Specification.Width, s_MaxFramebufferSize);
         VXM_CORE_ASSERT(m_Specification.Height > 0 && m_Specification.Height <= s_MaxFramebufferSize, "Framebufffer height '{0}'  must be superior to 0 and inferior to {1}.", m_Specification.Height, s_MaxFramebufferSize);
+
+		for (auto spec: m_Specification.Attachements.Attachments)
+		{
+			if(Utils::IsDepthFormat(spec.TextureFormat))
+			{
+				m_DepthAttachmentSpecification = spec;
+			}
+			else
+			{
+				m_ColorAttachmentSpecifications.push_back(spec);
+			}
+		}
 
         Invalidate();
     }
@@ -26,36 +111,88 @@ namespace Voxymore::Core {
 
     OpenGLFramebuffer::~OpenGLFramebuffer()
     {
-        glDeleteFramebuffers(1, &m_RendererID);
-        glDeleteTextures(1, &m_ColorAttachment);
-        glDeleteTextures(1, &m_DepthAttachment);
+		glDeleteFramebuffers(1, &m_RendererID);
+		glDeleteTextures(m_ColorAttachments.size(), m_ColorAttachments.data());
+		glDeleteTextures(1, &m_DepthAttachment);
     }
 
     void OpenGLFramebuffer::Invalidate() {
         if(m_RendererID)
         {
             glDeleteFramebuffers(1, &m_RendererID);
-            glDeleteTextures(1, &m_ColorAttachment);
+            glDeleteTextures(m_ColorAttachments.size(), m_ColorAttachments.data());
             glDeleteTextures(1, &m_DepthAttachment);
+			m_ColorAttachments.clear();
+			m_DepthAttachment = 0;
         }
 
         glCreateFramebuffers(1, &m_RendererID);
         glBindFramebuffer(GL_FRAMEBUFFER, m_RendererID);
 
-        glCreateTextures(GL_TEXTURE_2D, 1, &m_ColorAttachment);
-        glBindTexture(GL_TEXTURE_2D, m_ColorAttachment);
+		bool multisample = m_Specification.Samples > 1;
 
-        //TODO: put the GL_RGBA8 into the specification so that it's can be HDR compatible.
-        //TODO: Put the GL_RGBA into the specification because we might not need all the info.
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Specification.Width, m_Specification.Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_ColorAttachment, 0);
+		if(m_ColorAttachmentSpecifications.size())
+		{
+			m_ColorAttachments.resize(m_ColorAttachmentSpecifications.size());
 
-        glCreateTextures(GL_TEXTURE_2D, 1, &m_DepthAttachment);
-        glBindTexture(GL_TEXTURE_2D, m_DepthAttachment);
-        glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH24_STENCIL8, m_Specification.Width, m_Specification.Height);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_DepthAttachment, 0);
+			Utils::CreateTextures(multisample, m_ColorAttachments.data(), m_ColorAttachments.size());
+			for (size_t i = 0; i < m_ColorAttachments.size(); ++i)
+			{
+				auto spec = m_ColorAttachmentSpecifications[i];
+				Utils::BindTexture(multisample, m_ColorAttachments[i]);
+				switch (spec.TextureFormat)
+				{
+					case FramebufferTextureFormat::RGBA8:
+					{
+						Utils::AttachmentColorTexture(m_ColorAttachments[i], m_Specification.Samples, GL_RGBA8, m_Specification.Width, m_Specification.Height, i);
+						break;
+					}
+					case FramebufferTextureFormat::RGBA16:
+					{
+						Utils::AttachmentColorTexture(m_ColorAttachments[i], m_Specification.Samples, GL_RGBA16, m_Specification.Width, m_Specification.Height, i);
+						break;
+					}
+					default:
+					{
+						VXM_ASSERT(false, "The Texture Format {0} is not handled for the creation of a color texture.");
+					}
+
+				}
+			}
+		}
+
+		if(m_DepthAttachmentSpecification.TextureFormat != FramebufferTextureFormat::None)
+		{
+			Utils::CreateTextures(multisample, &m_DepthAttachment, 1);
+			Utils::BindTexture(multisample, m_DepthAttachment);
+			switch (m_DepthAttachmentSpecification.TextureFormat)
+			{
+				case FramebufferTextureFormat::DEPTH24STENCIL8:
+				{
+					Utils::AttachmentDepthTexture(m_DepthAttachment, m_Specification.Samples, GL_DEPTH24_STENCIL8, GL_DEPTH_STENCIL_ATTACHMENT, m_Specification.Width, m_Specification.Height);
+					break;
+				}
+				default:
+				{
+					VXM_ASSERT(false, "The Texture Format {0} is not handled for the creation of a depth texture.");
+				}
+			}
+		}
+
+		if(m_ColorAttachments.size() > 1)
+		{
+#define MAX_COLOR_ATTACHEMENT 5
+			VXM_ASSERT(m_ColorAttachments.size() <= MAX_COLOR_ATTACHEMENT, "We only support {0} color attachment on a framebuffer at the moment.", MAX_COLOR_ATTACHEMENT);
+
+			GLenum buffers[MAX_COLOR_ATTACHEMENT] = {GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2, GL_COLOR_ATTACHMENT3, GL_COLOR_ATTACHMENT4, GL_COLOR_ATTACHMENT5};
+
+			glDrawBuffers(m_ColorAttachments.size(), buffers);
+		}
+		else if(m_ColorAttachments.empty())
+		{
+			// Only Depth pass.
+			glDrawBuffer(GL_NONE);
+		}
 
         VXM_CORE_ASSERT(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE, "Framebuffer is incomplete.");
 
@@ -71,8 +208,8 @@ namespace Voxymore::Core {
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
     }
 
-    uint32_t OpenGLFramebuffer::GetColorAttachmentRendererID() const {
-        return m_ColorAttachment;
+    uint32_t OpenGLFramebuffer::GetColorAttachmentRendererID(uint32_t index) const {
+        return m_ColorAttachments[index];
     }
 
     uint32_t OpenGLFramebuffer::GetDepthAttachmentRendererID() const {
--- platform/OpenGL/Voxymore/OpenGL/OpenGLFramebuffer.hpp
+++ platform/OpenGL/Voxymore/OpenGL/OpenGLFramebuffer.hpp
@@ -12,8 +12,10 @@ namespace Voxymore::Core {
     private:
         FramebufferSpecification m_Specification;
         uint32_t m_RendererID = 0;
-        uint32_t m_ColorAttachment = 0;
-        uint32_t m_DepthAttachment = 0;
+		std::vector<uint32_t> m_ColorAttachments;
+		uint32_t m_DepthAttachment = 0;
+		std::vector<FramebufferTextureSpecification> m_ColorAttachmentSpecifications;
+		FramebufferTextureSpecification m_DepthAttachmentSpecification = FramebufferTextureFormat::None;
     private:
         void Invalidate();
     public:
@@ -24,7 +26,7 @@ namespace Voxymore::Core {
         virtual void Bind() override;
         virtual void Unbind() override;
         virtual void Resize(uint32_t width, uint32_t height) override;
-        virtual uint32_t GetColorAttachmentRendererID() const override;
+        virtual uint32_t GetColorAttachmentRendererID(uint32_t index = 0) const override;
         virtual uint32_t GetDepthAttachmentRendererID() const override;
     };
 
