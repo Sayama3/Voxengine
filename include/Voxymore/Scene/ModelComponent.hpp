04027705b548a2dd029d21eaf8f1ebc6c5328736
Iannis Poquet <ianpo@live.fr>
2023-10-11 23:23:47 +0200

    Add Shader changes

--- include/Voxymore/Scene/ModelComponent.hpp
+++ include/Voxymore/Scene/ModelComponent.hpp
@@ -16,6 +16,8 @@ namespace Voxymore::Core
         inline const std::filesystem::path& GetLocalPath() const {return m_ModelPath.path;}
 		inline void SetPath(const std::filesystem::path& p) { m_ModelPath.path = p;}
 		inline void SetPath(const Path& p) { m_ModelPath = p;}
+		inline void SetShader(const std::string& shader) { m_ShaderName = shader;}
+		inline const std::string& GetShader() const { return m_ShaderName;}
         inline const Ref<Model>& GetModel() const {return m_Model;}
         void LoadModel();
 		inline bool IsLoaded() const { return m_IsLoaded; }
