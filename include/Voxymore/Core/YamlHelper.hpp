10d1804c11127842c55ec32f609d44f6f4758e7e
Iannis Poquet <ianpo@live.fr>
2023-11-12 18:38:45 +0100

    Allow YAML Serialization of ivec2/3/4

--- include/Voxymore/Core/YamlHelper.hpp
+++ include/Voxymore/Core/YamlHelper.hpp
@@ -16,6 +16,9 @@ namespace Voxymore::Core
 	YAML::Emitter &operator<<(YAML::Emitter &out, const glm::vec2 &v);
 	YAML::Emitter &operator<<(YAML::Emitter &out, const glm::vec3 &v);
 	YAML::Emitter &operator<<(YAML::Emitter &out, const glm::vec4 &v);
+	YAML::Emitter &operator<<(YAML::Emitter &out, const glm::ivec2 &v);
+	YAML::Emitter &operator<<(YAML::Emitter &out, const glm::ivec3 &v);
+	YAML::Emitter &operator<<(YAML::Emitter &out, const glm::ivec4 &v);
 	YAML::Emitter &operator<<(YAML::Emitter &out, const glm::quat &v);
 	YAML::Emitter &operator<<(YAML::Emitter &out, const std::filesystem::path &v);
 }
@@ -78,6 +81,66 @@ namespace YAML
 			rhs.y = node[1].as<float>();
 			rhs.z = node[2].as<float>();
 			rhs.w = node[3].as<float>();
+			return true;
+		}
+	};
+	template<>
+	struct convert<glm::ivec2>
+	{
+		inline static Node encode(const glm::ivec2& rhs)
+		{
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			return node;
+		}
+		inline static bool decode(const Node& node, glm::ivec2& rhs)
+		{
+			if(!node.IsSequence() || node.size() != 2) return false;
+			rhs.x = node[0].as<int>();
+			rhs.y = node[1].as<int>();
+			return true;
+		}
+	};
+	template<>
+	struct convert<glm::ivec3>
+	{
+		inline static Node encode(const glm::ivec3& rhs)
+		{
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.push_back(rhs.z);
+			return node;
+		}
+		inline static bool decode(const Node& node, glm::ivec3& rhs)
+		{
+			if(!node.IsSequence() || node.size() != 3) return false;
+			rhs.x = node[0].as<int>();
+			rhs.y = node[1].as<int>();
+			rhs.z = node[2].as<int>();
+			return true;
+		}
+	};
+	template<>
+	struct convert<glm::ivec4>
+	{
+		inline static Node encode(const glm::ivec4& rhs)
+		{
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.push_back(rhs.z);
+			node.push_back(rhs.w);
+			return node;
+		}
+		inline static bool decode(const Node& node, glm::ivec4& rhs)
+		{
+			if(!node.IsSequence() || node.size() != 4) return false;
+			rhs.x = node[0].as<int>();
+			rhs.y = node[1].as<int>();
+			rhs.z = node[2].as<int>();
+			rhs.w = node[3].as<int>();
 			return true;
 		}
 	};
--- src/Core/YamlHelper.cpp
+++ src/Core/YamlHelper.cpp
@@ -25,6 +25,26 @@ namespace Voxymore::Core
 		out << YAML::BeginSeq << v.x << v.y << v.z << v.w << YAML::EndSeq;
 		return out;
 	}
+	YAML::Emitter &operator<<(YAML::Emitter &out, const glm::ivec2 &v)
+	{
+		out << YAML::Flow;
+		out << YAML::BeginSeq << v.x << v.y << YAML::EndSeq;
+		return out;
+	}
+
+	YAML::Emitter &operator<<(YAML::Emitter &out, const glm::ivec3 &v)
+	{
+		out << YAML::Flow;
+		out << YAML::BeginSeq << v.x << v.y << v.z << YAML::EndSeq;
+		return out;
+	}
+
+	YAML::Emitter &operator<<(YAML::Emitter &out, const glm::ivec4 &v)
+	{
+		out << YAML::Flow;
+		out << YAML::BeginSeq << v.x << v.y << v.z << v.w << YAML::EndSeq;
+		return out;
+	}
 
 	YAML::Emitter &operator<<(YAML::Emitter &out, const glm::quat &v)
 	{
