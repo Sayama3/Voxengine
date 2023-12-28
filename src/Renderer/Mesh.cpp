4bb1ebd630b56820265f570013c1f4117ae6ff3f
Iannis Poquet <ianpo@live.fr>
2023-12-28 17:22:53 +0100

    Fix Up & Down face on cube

--- src/Renderer/Mesh.cpp
+++ src/Renderer/Mesh.cpp
@@ -197,12 +197,12 @@ namespace Voxymore::Core
 					Vertex(glm::vec3(+0.5f, -0.5f, -0.5f), glm::vec3(0,0,-1), glm::vec2(1,0)), // 5
 					Vertex(glm::vec3(+0.5f, +0.5f, -0.5f), glm::vec3(0,0,-1), glm::vec2(1,1)), // 6
 					Vertex(glm::vec3(-0.5f, +0.5f, -0.5f), glm::vec3(0,0,-1), glm::vec2(0,1)), // 7
-					//Front Face
+					//Up Face
 					Vertex(glm::vec3(-0.5f, +0.5f, -0.5f), glm::vec3(0,+1, 0), glm::vec2(0,0)), // 8
 					Vertex(glm::vec3(+0.5f, +0.5f, -0.5f), glm::vec3(0,+1, 0), glm::vec2(1,0)), // 9
 					Vertex(glm::vec3(+0.5f, +0.5f, +0.5f), glm::vec3(0,+1, 0), glm::vec2(1,1)), // 10
 					Vertex(glm::vec3(-0.5f, +0.5f, +0.5f), glm::vec3(0,+1, 0), glm::vec2(0,1)), // 11
-					//Front Face
+					//Down Face
 					Vertex(glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(0,-1, 0), glm::vec2(0,0)), // 12
 					Vertex(glm::vec3(+0.5f, -0.5f, -0.5f), glm::vec3(0,-1, 0), glm::vec2(1,0)), // 13
 					Vertex(glm::vec3(+0.5f, -0.5f, +0.5f), glm::vec3(0,-1, 0), glm::vec2(1,1)), // 14
@@ -227,12 +227,12 @@ namespace Voxymore::Core
 					// Back
 					4,6,5,
 					4,7,6,
-					// Front
-					8,9,10,
-					8,10,11,
-					// Back
-					12,14,13,
-					12,15,14,
+					// Up
+					8,10,9,
+					8,11,10,
+					// Down
+					12,13,14,
+					12,14,15,
 					// Front
 					16,17,18,
 					16,18,19,
