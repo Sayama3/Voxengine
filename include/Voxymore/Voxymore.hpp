cb4952fa20cbc4c2bbcc8fccbad1f199afa34d84
Iannis Poquet <ianpo@live.fr>
2024-01-05 20:46:24 +0100

    Add ParticleComponent to "Voxymore.hpp"

--- include/Voxymore/Voxymore.hpp
+++ include/Voxymore/Voxymore.hpp
@@ -35,6 +35,7 @@
 #include "Voxymore/Components/CustomComponent.hpp"
 #include "Voxymore/Components/ModelComponent.hpp"
 #include "Voxymore/Components/PrimitiveComponent.hpp"
+#include "Voxymore/Physics/ParticleComponent.hpp"
 
 #include "Voxymore/Scene/Entity.hpp"
 #include "Voxymore/Scene/GameplaySystem.hpp"
