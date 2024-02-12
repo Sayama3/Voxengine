1448b88680ca738e4aa67d0c6981c2908f729624
Iannis Poquet <ianpo@live.fr>
2024-02-12 13:06:52 +0100

    Add Getter/Setters & documentation for the rigidbody

--- Core/include/Voxymore/Physics/Rigidbodies/Rigidbody.hpp
+++ Core/include/Voxymore/Physics/Rigidbodies/Rigidbody.hpp
@@ -10,6 +10,16 @@
 
 namespace Voxymore::Core
 {
+	/**
+	 * @class Rigidbody
+	 *
+	 * @brief Represents a rigid body in a physics simulation.
+	 *
+	 * The Rigidbody class is used to represent a physical object in a physics simulation.
+	 * It stores various properties of the object such as its mass, position, orientation,
+	 * velocity, and acceleration. It also provides methods for applying forces, calculating
+	 * the transformation matrix, and manipulating the body's inertia tensor.
+	 */
 	class Rigidbody
 	{
 	public:
@@ -20,10 +30,44 @@ namespace Voxymore::Core
 		void SetInverseInertiaTensor(const Mat3& inverseInertiaTensor);
 
 		void AddForce(const Vec3& force);
+		void AddAcceleration(const Vec3& acceleration);
 		void ClearAccumulator();
 
 		void AddForceAtPoint(const Vec3& force,const Vec3& point);
 		void AddForceAtBodyPoint(const Vec3& force,const Vec3& bodyPoint);
+
+		void AddAccelerationAtPoint(const Vec3& accel, const Vec3& point);
+		void AddAccelerationAtBodyPoint(const Vec3& accel, const Vec3& bodyPoint);
+
+		[[nodiscard]] Real GetInverseMass() const;
+		Real& GetInverseMass();
+		void SetInverseMass(Real inverseMass);
+
+		[[nodiscard]] Real GetMass() const;
+		void SetMass(Real mass);
+
+		[[nodiscard]] bool HasFiniteMass() const;
+
+		[[nodiscard]] Real GetLinearDamping() const;
+		Real& GetLinearDamping();
+		void SetLinearDamping(Real linearDamping);
+
+		[[nodiscard]] const Vec3& GetPosition() const;
+		Vec3& GetPosition();
+		void SetPosition(const Vec3& position);
+
+		[[nodiscard]] const Quat& GetOrientation() const;
+		Quat& GetOrientation();
+		void SetOrientation(const Quat& orientation);
+
+		[[nodiscard]] const Vec3& GetLinearVelocity() const;
+		Vec3& GetLinearVelocity();
+		void SetLinearVelocity(const Vec3& linearVelocity);
+
+		[[nodiscard]] const Vec3& GetAngularVelocity() const;
+		Vec3& GetAngularVelocity();
+		void SetAngularVelocity(const Vec3& angularVelocity);
+
 	protected:
 		/**
 		 * Hold the inverse of the mass of the rigidbody. It is
@@ -75,10 +119,51 @@ namespace Voxymore::Core
 		 */
 		Mat3 m_InverseInertiaTensor;
 
+		/**
+		 *   @brief Used to accumulate forces applied on the Rigidbody during a physics simulation step.
+		 *
+		 *   The 3-dimensional vector m_ForceAccumulate represents the total force currently applied to the rigid body.
+		 *   This force is an accumulation of all the individual forces applied to the body.
+		 *
+		 *   During a simulation step, external entities or the physics engine apply forces to the body.
+		 *   These forces are added (accumulated) to m_ForceAccumulate. This is based on the principle
+		 *   that forces are additive - the net effect of multiple forces acting on a body
+		 *   is the sum of their individual effects.
+		 *
+		 *   This force accumulation process allows for the resultant force to be calculated in one go before
+		 *   the physics simulation uses it to update the rigid body's state (position, velocity).
+		 *
+		 *   Typically, after each simulation step, m_ForceAccumulate is cleared to accept a new set of forces
+		 *   for the next simulation step.
+		 */
 		Vec3 m_ForceAccumulate;
 
+		/**
+		 * @brief Accumulation of torques applied to the Rigidbody during a physics simulation step.
+		 *
+		 * This Vec3 data member represents the total torque applied to the Rigidbody during the simulation step.
+		 * Torque, in physics, is a measure of the force that can cause an object to rotate about an axis.
+		 *
+		 * The torques resulting from forces applied at specific points on the body during the physics simulation are accumulated into m_TorqueAccumulate.
+		 * Once the simulation step is done, the accumulated torque is used to update the state of the Rigidbody, specifically its angular velocity.
+		 *
+		 * Typically, m_TorqueAccumulate is cleared after each simulation step so it can accept and accumulate new torques in the next step.
+		 * The clearing is done by calling the 'ClearAccumulator()' method in the physics engine updates.
+		 */
 		Vec3 m_TorqueAccumulate;
 
+		/**
+		 * @brief Flag to check if the Rigidbody is awake in the simulation.
+		 *
+		 * This Boolean data member, m_IsAwake, represents the awake state of the Rigidbody during the physics simulation step.
+		 * When a Rigidbody is awake, it is subject to the physics simulation, which includes handling forces, collisions, etc.
+		 *
+		 * If m_IsAwake is set to true, this indicates that the Rigidbody is active in the physics simulation and needs to be updated.
+		 * This might be due to a force or torque being applied, or the Rigidbody might be in motion due to previous simulation steps.
+		 *
+		 * If m_IsAwake is set to false, the physics engine may choose to omit this Rigidbody from certain computations, improving performance.
+		 * Note: The specific behavior may vary depending on the physics engine's design and implementation.
+		 */
 		bool m_IsAwake;
 	};
 
--- Core/src/Physics/Rigidbodies/Rigidbody.cpp
+++ Core/src/Physics/Rigidbodies/Rigidbody.cpp
@@ -32,6 +32,12 @@ namespace Voxymore::Core
 		m_IsAwake = true;
 	}
 
+	void Rigidbody::AddAcceleration(const Vec3 & accel)
+	{
+		m_ForceAccumulate += HasFiniteMass() ? accel * GetMass() : accel;
+		m_IsAwake = true;
+	}
+
 
 	void Rigidbody::AddForceAtPoint(const Vec3& force,const Vec3& point)
 	{
@@ -46,7 +52,22 @@ namespace Voxymore::Core
 
 	void Rigidbody::AddForceAtBodyPoint(const Vec3& force,const Vec3& bodyPoint)
 	{
-		AddForceAtPoint(force, CalculateTransformMatrix() * Vec4(bodyPoint,1));
+		auto pointv4 = Vec4(bodyPoint,1);
+		auto transform = CalculateTransformMatrix();
+		auto point = transform * pointv4;
+		AddForceAtPoint(force, point);
+	}
+
+	void Rigidbody::AddAccelerationAtPoint(const Vec3& accel,const Vec3& point)
+	{
+		auto force = HasFiniteMass() ? accel * GetMass() : accel;
+		AddForceAtPoint(force, point);
+	}
+
+	void Rigidbody::AddAccelerationAtBodyPoint(const Vec3& accel,const Vec3& bodyPoint)
+	{
+		auto force = HasFiniteMass() ? accel * GetMass() : accel;
+		AddForceAtBodyPoint(force, bodyPoint);
 	}
 
 	void Rigidbody::ClearAccumulator()
@@ -54,4 +75,109 @@ namespace Voxymore::Core
 		m_ForceAccumulate = Vec3(0, 0, 0);
 		m_TorqueAccumulate = Vec3(0, 0, 0);
 	}
+
+	Real Rigidbody::GetInverseMass() const
+	{
+		return m_InverseMass;
+	}
+
+	Real& Rigidbody::GetInverseMass()
+	{
+		return m_InverseMass;
+	}
+
+	void Rigidbody::SetInverseMass(Real inverseMass)
+	{
+		m_InverseMass = inverseMass;
+	}
+	Real Rigidbody::GetMass() const
+	{
+		return ((Real)1) / m_InverseMass;
+	}
+
+	void Rigidbody::SetMass(Real mass)
+	{
+		m_InverseMass = ((Real)1) / mass;
+	}
+
+	bool Rigidbody::HasFiniteMass() const
+	{
+		return m_InverseMass > 0;
+	}
+
+	Real Rigidbody::GetLinearDamping() const
+	{
+		return m_LinearDamping;
+	}
+
+	Real& Rigidbody::GetLinearDamping()
+	{
+		return m_LinearDamping;
+	}
+
+	void Rigidbody::SetLinearDamping(Real linearDamping)
+	{
+		m_LinearDamping = linearDamping;
+	}
+
+	const Vec3& Rigidbody::GetPosition() const
+	{
+		return m_Position;
+	}
+
+	Vec3& Rigidbody::GetPosition()
+	{
+		return m_Position;
+	}
+
+	void Rigidbody::SetPosition(const Vec3& position)
+	{
+		m_Position = position;
+	}
+
+	const Quat& Rigidbody::GetOrientation() const
+	{
+		return m_Orientation;
+	}
+
+	Quat& Rigidbody::GetOrientation()
+	{
+		return m_Orientation;
+	}
+
+	void Rigidbody::SetOrientation(const Quat& orientation)
+	{
+		m_Orientation = orientation;
+	}
+
+	const Vec3& Rigidbody::GetLinearVelocity() const
+	{
+		return m_LinearVelocity;
+	}
+
+	Vec3& Rigidbody::GetLinearVelocity()
+	{
+		return m_LinearVelocity;
+	}
+
+	void Rigidbody::SetLinearVelocity(const Vec3& linearVelocity)
+	{
+		m_LinearVelocity = linearVelocity;
+	}
+
+	const Vec3& Rigidbody::GetAngularVelocity() const
+	{
+		return m_AngularVelocity;
+	}
+
+	Vec3& Rigidbody::GetAngularVelocity()
+	{
+		return m_AngularVelocity;
+	}
+
+	void Rigidbody::SetAngularVelocity(const Vec3& angularVelocity)
+	{
+		m_AngularVelocity = angularVelocity;
+	}
+
 } // namespace Voxymore::Core
