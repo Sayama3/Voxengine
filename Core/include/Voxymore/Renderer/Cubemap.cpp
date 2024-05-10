8e6c3287d686efdfd74436f170eab00b5cd8ae37
Iannis Poquet <ianpo@live.fr>
2024-05-10 21:33:59 +0200

    Create cubemap.hpp/cpp

---
+++ Core/include/Voxymore/Renderer/Cubemap.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Iannis on 10/05/2024.
+//
+
+#include "Cubemap.hpp"
+
+
+namespace Voxymore::Core
+{
+} // namespace Voxymore::Core
---
+++ Core/include/Voxymore/Renderer/Cubemap.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Iannis on 10/05/2024.
+//
+
+#pragma once
+
+
+namespace Voxymore::Core
+{
+
+	class Cubemap
+	{
+	};
+
+} // namespace Voxymore::Core
+
