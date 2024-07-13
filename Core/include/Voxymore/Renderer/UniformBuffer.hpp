054f6c0517c741d513b3953a27b3a03865e536e0
Iannis Poquet <ianpo@live.fr>
2024-07-13 17:07:45 +0200

    Add template methods to UniformBuffer class
    The implemented changes introduce two new template methods to the UniformBuffer class. The SetData method ensures data of any type can be set more conveniently, and the Create method improves the ease of UniformBuffer object creation. These enhancements provide greater flexibility in the use of the UniformBuffer class.

--- Core/include/Voxymore/Renderer/UniformBuffer.hpp
+++ Core/include/Voxymore/Renderer/UniformBuffer.hpp
@@ -15,8 +15,12 @@ namespace Voxymore::Core
 	public:
 		virtual ~UniformBuffer() {}
 		virtual void SetData(const void* data, uint32_t size, uint32_t offset = 0) = 0;
+		template<typename T>
+		void SetData(const T* data) {SetData(data, sizeof(T), 0);}
 
 		static Ref<UniformBuffer> Create(uint32_t size, uint32_t binding);
+		template<typename T>
+		static Ref<UniformBuffer> Create(uint32_t binding) {return Create(sizeof(T), binding);}
 	};
 
 } // namespace Voxymore::Core
