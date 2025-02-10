3fc05a221dd03dc8e83561c697a69fb570874bce
Iannis Poquet <ianpo@live.fr>
2025-02-10 23:38:35 +0100

    [WIP] Start terrain

---
+++ Core/include/Voxymore/Terrain/Terrain.hpp
@@ -0,0 +1,37 @@
+//
+// Created by ianpo on 07/02/2025.
+//
+
+#pragma once
+
+#include "Voxymore/Core/SmartPointers.hpp"
+#include "Voxymore/Math/Math.hpp"
+#include "Voxymore/Renderer/Buffer.hpp"
+#include "Voxymore/Renderer/Shader.hpp"
+
+namespace Voxymore::Core {
+
+    class Terrain {
+    public:
+        using Vec3Size = glm::vec<3,uint32_t>;
+        using Vec3Position = glm::vec<3,int64_t>;
+        using Voxel = uint8_t;
+        inline static constexpr Vec3Size ChunkWidth{32,32,32};
+        inline static constexpr uint32_t VoxelCountInChunk{ChunkWidth.x*ChunkWidth.y*ChunkWidth.z};
+        using Chunk = std::array<Voxel, VoxelCountInChunk>;
+    public:
+        Terrain() = delete;
+        ~Terrain() = delete;
+        Terrain(const Terrain&) = delete;
+        Terrain& operator=(const Terrain&) = delete;
+    public:
+        static void Initialize();
+        static void Destroy();
+    private:
+        inline static Ref<SSBO> s_SSBO = nullptr;
+        inline static Ref<Shader> s_RenderShader = nullptr;
+        inline static Ref<ComputeShader> s_ComputeShader = nullptr;
+        inline static std::unordered_map<Vec3Position, Chunk> s_Chunks{{Vec3Position{0},{}}};
+    };
+
+} // Voxymore::Core
---
+++ Core/src/Terrain/Terrain.cpp
@@ -0,0 +1,19 @@
+//
+// Created by ianpo on 07/02/2025.
+//
+
+#include "Voxymore/Terrain/Terrain.hpp"
+
+namespace Voxymore::Core {
+    void Terrain::Initialize() {
+        s_Chunks.insert_or_assign(Vec3Position{0}, Chunk{1});
+        s_SSBO = SSBO::Create(VoxelCountInChunk * sizeof(Voxel), SSBO::DynamicDraw);
+    }
+
+    void Terrain::Destroy() {
+        s_SSBO.reset();
+        s_RenderShader.reset();
+        s_ComputeShader.reset();
+        s_Chunks.clear();
+    }
+} // Voxymore::Core
